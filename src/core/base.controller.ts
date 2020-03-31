882c66a97d3dca4e38fac4bcf0f127222cc928d4
zhaojintian <729234283@qq.com>
2020-03-31 18:57:27 +0800

    UPDATE

--- src/core/base.controller.ts
+++ src/core/base.controller.ts
@@ -1,8 +1,11 @@
 import * as jwt from "jsonwebtoken";
 import { JwtOptionsInterface } from "../interfaces/jwt-options.interface";
+import { OrderAction } from "../enums/order-action.enum";
 
 export class BaseController {
   public jwtConfig: JwtOptionsInterface;
+
+  public repository: any;
 
   public include: Object;
 
@@ -19,5 +22,77 @@ export class BaseController {
       (authorization || "").substring(7),
       this.jwtConfig.secret
     );
+  }
+
+  async findPrev({
+    id = "",
+    attributes = null,
+    where = null,
+    order = [["order", "ASC"]]
+  } = {}) {
+    if (id) {
+      where = { ...where, id: { $gt: id } };
+    }
+    return (
+      (await this.repository.findAll({ attributes, where, order }))[0] || null
+    );
+  }
+
+  async findNext({
+    id = "",
+    attributes = null,
+    where = null,
+    order = [["order", "DESC"]]
+  } = {}) {
+    if (id) {
+      where = { ...where, id: { $lt: id } };
+    }
+    return (
+      (await this.repository.findAll({ attributes, where, order }))[0] || null
+    );
+  }
+
+  async order({ id, action, where = null }): Promise<void> {
+    const findByPkRes = await this.repository.findByPk(id);
+    const findPrevRes = await this.findPrev({
+      id,
+      where: {
+        ...where,
+        order: { $gt: findByPkRes.order }
+      }
+    });
+    const findNextRes = await this.findNext({
+      id,
+      where: {
+        ...where,
+        order: { $lt: findByPkRes.order }
+      }
+    });
+
+    if (action === OrderAction.ToPrev && findPrevRes) {
+      await this.repository.update(
+        { order: findPrevRes.order },
+        {
+          where: { id }
+        }
+      );
+
+      await this.repository.update(
+        { order: findByPkRes.order },
+        {
+          where: { id: findPrevRes.id }
+        }
+      );
+    } else if (action === OrderAction.ToNext && findPrevRes) {
+      await this.repository.update(
+        { order: findNextRes.order },
+        { where: { id } }
+      );
+
+      await this.repository.update(
+        { order: findByPkRes.order },
+        { where: { id: findNextRes.id } }
+      );
+    }
   }
 }
