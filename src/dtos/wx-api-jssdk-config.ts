c4e2a4188e356b2d5e3048039b3e97b6cc4daa36
zhaojintian <729234283@qq.com>
2020-04-13 21:58:04 +0800

    update

--- src/dtos/wx-api-jssdk-config.ts
+++ src/dtos/wx-api-jssdk-config.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-class WxApiJsSdkConfigDto {
+export class WxApiJsSdkConfigDto {
   @ApiProperty({
     description: "当前页面 URL",
     example: "http://www.baidu.com/"
