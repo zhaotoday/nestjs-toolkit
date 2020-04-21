39bb2a25f86079ebdd45d035431dcad4ca4e17af
zhaojintian <729234283@qq.com>
2020-04-22 05:59:44 +0800

    update

---
+++ src/entities/brand.entity.ts
@@ -0,0 +1,47 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { Is } from "../enums/is.enum";
+
+@Table({
+  tableName: "brands"
+})
+export class Brand extends Model<Brand> {
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    comment: "ID"
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING(100),
+    comment: "名称"
+  })
+  name: string;
+
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    comment: "Logo ID"
+  })
+  logoId: number;
+
+  @Column({
+    type: DataType.STRING(500),
+    comment: "链接"
+  })
+  link: string;
+
+  @Column({
+    type: DataType.TINYINT({ length: 1 }),
+    comment: "状态",
+    defaultValue: Is.True
+  })
+  status: number;
+
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    comment: "排序"
+  })
+  order: number;
+}
