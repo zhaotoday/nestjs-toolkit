e541a96560447b5d0311a5f9d61b7b4210dde3bc
zhaojintian <729234283@qq.com>
2020-05-02 17:15:21 +0800

    update

--- src/utils/image.util.ts
+++ src/utils/image.util.ts
@@ -1,12 +1,4 @@
 import { Injectable } from "@nestjs/common";
 
-const Jimp = require("jimp");
-
 @Injectable()
-export class ImageUtil {
-  public Image: any;
-
-  constructor() {
-    this.Image = Jimp;
-  }
-}
+export class ImageUtil {}
