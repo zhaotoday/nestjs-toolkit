bdb018480463c0585a3d31070d56ab7a258c18f9
zhaojintian <729234283@qq.com>
2020-05-17 16:31:10 +0800

    update

---
+++ src/entities/distributor-application.entity.ts
@@ -0,0 +1,23 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { CheckStatus } from "../enums/check-status.enum";
+
+@Table({
+  tableName: "distributorApplications"
+})
+export class DistributorApplication extends Model<DistributorApplication> {
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    comment: "ID"
+  })
+  id: number;
+
+  @Column({
+    type: DataType.TINYINT({ length: 1 }),
+    comment: "审核状态",
+    defaultValue: CheckStatus.Checking
+  })
+  status: number;
+}
