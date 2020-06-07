d5b9f2b4d54e7f5015c8338808300fcb992776d9
zhaojintian <729234283@qq.com>
2020-06-07 20:27:41 +0800

    u

--- src/dtos/wx-user.dto.ts
+++ src/dtos/wx-user.dto.ts
@@ -14,110 +14,113 @@ export class WxUserDto {
     description: "OPEN ID",
     example: "omMee4tymy2V-axAKL5EpKwUuWG8"
   })
-  readonly openId: string;
+  openId: string;
 
   @ApiProperty({
     description: "公众号 OPEN ID",
     example: "omMee4tymy2V-axAKL5EpKwUuWG8"
   })
-  readonly oaOpenId: string;
+  @ApiPropertyOptional()
+  oaOpenId?: string;
 
   @ApiProperty({
     description: "网站应用 OPEN ID",
     example: "omMee4tymy2V-axAKL5EpKwUuWG8"
   })
-  readonly webOpenId: string;
+  @ApiPropertyOptional()
+  webOpenId?: string;
 
   @ApiProperty({
     description: "移动应用 OPEN ID",
     example: "omMee4tymy2V-axAKL5EpKwUuWG8"
   })
-  readonly mobileOpenId: string;
+  @ApiPropertyOptional()
+  mobileOpenId?: string;
 
   @ApiProperty({
     description: "UNION ID",
     example: "o-2fp5rjHIq5xE52gGwriP1qFhf8"
   })
   @ApiPropertyOptional()
-  readonly unionId?: string;
+  unionId?: string;
 
   @ApiProperty({
     description: "头像",
     example:
       "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eopRYpiak5MdicbcLY43zibSP1HTnqU6yHhqnQ614sxV6slPpxJzgXfuzJicneSDEssjicqOkiaIiawTF3dw/132"
   })
-  readonly avatarUrl: string;
+  avatarUrl: string;
 
   @ApiProperty({
     description: "自定义头像 ID",
     example: 1
   })
   @ApiPropertyOptional()
-  readonly avatarId?: number;
+  avatarId?: number;
 
   @ApiProperty({
     description: "姓名",
     example: "赵金添"
   })
   @ApiPropertyOptional()
-  readonly name?: string;
+  name?: string;
 
   @ApiProperty({
     description: "昵称",
     example: "心晴"
   })
-  readonly nickName: string;
+  nickName: string;
 
   @ApiProperty({
     description: "性别",
     example: 1
   })
-  readonly gender: number;
+  gender: number;
 
   @ApiProperty({
     description: "手机号",
     example: "13950442340"
   })
   @ApiPropertyOptional()
-  readonly phoneNumber?: string;
+  phoneNumber?: string;
 
   @ApiProperty({
     description: "生日",
     example: "1985-02-05"
   })
   @ApiPropertyOptional()
-  readonly birthday?: string;
+  birthday?: string;
 
   @ApiProperty({
     description: "语言",
     example: "zh_CN"
   })
-  readonly language: string;
+  language: string;
 
   @ApiProperty({
     description: "国家",
     example: "China"
   })
-  readonly country: string;
+  country: string;
 
   @ApiProperty({
     description: "省份",
     example: "Fujian"
   })
-  readonly province: string;
+  province: string;
 
   @ApiProperty({
     description: "城市",
     example: "Fuzhou"
   })
-  readonly city: string;
+  city: string;
 
   @ApiProperty({
     description: "所有数据",
     example: {}
   })
   @ApiPropertyOptional()
-  readonly data?: string;
+  data?: string;
 
   @ApiProperty({
     description: "用户类型",
@@ -131,5 +134,5 @@ export class WxUserDto {
     example: Is.True
   })
   @ApiPropertyOptional()
-  readonly status?: number;
+  status?: number;
 }
