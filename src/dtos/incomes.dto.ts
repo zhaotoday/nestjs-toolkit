023485dbd362809c3957f226fd337c231fdc1d76
zhaojintian <729234283@qq.com>
2020-06-16 06:39:07 +0800

    u

--- src/dtos/incomes.dto.ts
+++ src/dtos/incomes.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 
-export class CommissionDto {
+export class IncomesDto {
   @ApiProperty({
     description: "ID",
     example: 1
@@ -15,19 +15,13 @@ export class CommissionDto {
   wxUserId: number;
 
   @ApiProperty({
-    description: "下级微信用户 ID（分销关系）",
-    example: 1
-  })
-  lowerWxUserId: number;
-
-  @ApiProperty({
     description: "订单 ID",
     example: 1
   })
   orderId: number;
 
   @ApiProperty({
-    description: "佣金金额",
+    description: "收入金额",
     example: 100
   })
   value: number;
---
+++ src/entities/incomes.entity.ts
@@ -0,0 +1,22 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+@Table({
+  tableName: "incomes"
+})
+export class Incomes extends Model<Incomes> {
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    comment: "ID"
+  })
+  id: number;
+
+  @Column({
+    type: DataType.FLOAT(10),
+    comment: "收入金额",
+    defaultValue: 0
+  })
+  value: number;
+}
