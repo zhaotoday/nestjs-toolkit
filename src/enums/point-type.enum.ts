0b3608f7cc1031685e1290f231b0beecd3741810
zhaojintian <729234283@qq.com>
2020-06-21 18:41:56 +0800

    u

--- src/enums/point-type.enum.ts
+++ src/enums/point-type.enum.ts
@@ -1,6 +1,7 @@
 export enum PointType {
   BuyProductDeducting = "BuyProductDeducting",
   BuyProductGiving = "BuyProductGiving",
+  SellProduct = "SellProduct",
   IncreaseOrDecrease = "IncreaseOrDecrease",
   Commission = "Commission"
 }
