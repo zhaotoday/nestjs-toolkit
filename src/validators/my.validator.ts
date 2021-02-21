482a25484504f9b68cf602b9b8891e06ef881bb3
729234283 <729234283@qq.com>
2021-02-21 13:23:17 +0800

    update

---
+++ src/validators/my.validator.ts
@@ -0,0 +1,18 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from "class-validator";
+
+@ValidatorConstraint()
+export class MyValidator implements ValidatorConstraintInterface {
+  validate(text: string, args: ValidationArguments) {
+    return (
+      text.length >= args.constraints[0] && text.length <= args.constraints[1]
+    );
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `${args.constraints[0]}-${args.constraints[1]}`;
+  }
+}
