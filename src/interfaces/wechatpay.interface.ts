a0979c103bc7e3303cba1249dd8923e188318522
zhaojintian <729234283@qq.com>
2021-07-30 17:10:40 +0800

    update

--- src/interfaces/wechatpay.interface.ts
+++ src/interfaces/wechatpay.interface.ts
@@ -1,9 +1,11 @@
 import { MpInterface } from "./mp.interface";
 import { MerchantInterface } from "./merchant.interface";
 import { OaInterface } from "./oa.interface";
+import { AppInterface } from "./app.interface";
 
 export interface WeChatPayInterface {
   mp?: MpInterface;
   oa?: OaInterface;
+  app?: AppInterface;
   merchant: MerchantInterface;
 }
