3b4468c8473dea61cb1f82f4cfaee63bf8223cce
zhaojintian <729234283@qq.com>
2021-07-30 17:08:20 +0800

    update

--- src/shared/wechatpay.provider.ts
+++ src/shared/wechatpay.provider.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import * as WeChatPay from "wechatpay.js";
 import { WeChatPayInterface } from "../interfaces/wechatpay.interface";
 import { Request } from "express";
-import { WxUserLoginType } from "..";
+import { WxUserLoginType } from "../enums/wx-user-login-type.enum";
 
 @Injectable()
 export class WeChatPayProvider {
@@ -16,12 +16,23 @@ export class WeChatPayProvider {
   }
 
   init({ type = WxUserLoginType.Mp } = {}): WeChatPayProvider {
-    const { mp, oa, merchant } = this._config;
+    const { mp, oa, app, merchant } = this._config;
 
     this.weChatPay = new WeChatPay({
-      appid: type === WxUserLoginType.Mp ? mp.appId : oa.appId,
+      appid: ((type) => {
+        switch (type) {
+          case WxUserLoginType.Mp:
+            return mp.appId;
+          case WxUserLoginType.Oa:
+            return oa.appId;
+          case WxUserLoginType.App:
+            return app.appId;
+          default:
+            return "";
+        }
+      })(),
       mch_id: merchant.mchId,
-      key: merchant.secret
+      key: merchant.secret,
     });
 
     return this;
@@ -35,7 +46,7 @@ export class WeChatPayProvider {
       out_trade_no: outTradeNo,
       total_fee: amount * 100,
       spbill_create_ip: "123.12.12.123",
-      notify_url: notifyUrl
+      notify_url: notifyUrl,
     });
 
     return { unifiedOrder, extra };
@@ -47,7 +58,7 @@ export class WeChatPayProvider {
       partner_trade_no: partnerTradeNo,
       amount: amount * 100,
       desc,
-      spbill_create_ip: "123.12.12.123"
+      spbill_create_ip: "123.12.12.123",
     });
   }
 
