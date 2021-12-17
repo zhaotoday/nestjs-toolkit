7e478fe1764be473830d8382783dbf28e93085fe
zhaojintian <729234283@qq.com>
2021-12-18 07:17:07 +0800

    u

--- src/dtos/file.dto.ts
+++ src/dtos/file.dto.ts
@@ -3,44 +3,50 @@ import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 export class FileDto {
   @ApiProperty({
     description: "ID",
-    example: 1
+    example: 1,
   })
   @ApiPropertyOptional()
   id?: number;
 
   @ApiProperty({
     description: "UUID",
-    example: "5e054c72-6e5f-4288-974c-840d801576ea"
+    example: "5e054c72-6e5f-4288-974c-840d801576ea",
   })
   readonly uuid: string;
 
   @ApiProperty({
     description: "名称",
-    example: "xxx.png"
+    example: "xxx.png",
   })
   readonly name: string;
 
   @ApiProperty({
     description: "类型",
-    example: "image/png"
+    example: "image/png",
   })
   readonly type: string;
 
   @ApiProperty({
     description: "大小",
-    example: "1024"
+    example: "1024",
   })
   readonly size: string;
 
   @ApiProperty({
     description: "目录",
-    example: "2020-01-01"
+    example: "videos/1",
   })
   readonly dir: string;
 
   @ApiProperty({
+    description: "日期",
+    example: "2020-01-01",
+  })
+  readonly date: string;
+
+  @ApiProperty({
     description: "后缀",
-    example: "png"
+    example: "png",
   })
   readonly ext: string;
 }
--- src/entities/file.entity.ts
+++ src/entities/file.entity.ts
@@ -2,7 +2,7 @@ import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 @Table({
   tableName: "files",
-  comment: "文件"
+  comment: "文件",
 })
 export class File extends Model {
   @Column({
@@ -10,43 +10,49 @@ export class File extends Model {
     primaryKey: true,
     autoIncrement: true,
     allowNull: false,
-    comment: "ID"
+    comment: "ID",
   })
   id: number;
 
   @Column({
     type: DataType.STRING(36),
-    comment: "UUID"
+    comment: "UUID",
   })
   uuid: string;
 
   @Column({
     type: DataType.STRING(200),
-    comment: "名称"
+    comment: "名称",
   })
   name: string;
 
   @Column({
     type: DataType.STRING(50),
-    comment: "类型"
+    comment: "类型",
   })
   type: string;
 
   @Column({
     type: DataType.STRING(50),
-    comment: "大小"
+    comment: "大小",
   })
   size: string;
 
   @Column({
-    type: DataType.STRING(10),
-    comment: "目录"
+    type: DataType.STRING(50),
+    comment: "目录",
   })
   dir: string;
 
   @Column({
     type: DataType.STRING(10),
-    comment: "后缀"
+    comment: "日期",
+  })
+  date: string;
+
+  @Column({
+    type: DataType.STRING(10),
+    comment: "后缀",
   })
   ext: string;
 }
