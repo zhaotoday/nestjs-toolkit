2aca0cde57ab5e316047603a35d8d96cd9bc261b
zhaojintian <729234283@qq.com>
2022-03-07 10:20:19 +0800

    u

--- src/dtos/product.dto.ts
+++ src/dtos/product.dto.ts
@@ -4,104 +4,111 @@ import { Is } from "../enums/is.enum";
 export class ProductDto {
   @ApiProperty({
     description: "ID",
-    example: 1
+    example: 1,
   })
   @ApiPropertyOptional()
   id?: number;
 
   @ApiProperty({
     description: "分类 ID",
-    example: 1
+    example: 1,
   })
   categoryId: number;
 
   @ApiProperty({
     description: "名称",
-    example: "回锅肉"
+    example: "回锅肉",
   })
   name: string;
 
   @ApiProperty({
     description: "价格",
-    example: 100.01
+    example: 100.01,
   })
   price: number;
 
   @ApiProperty({
     description: "原价",
-    example: 200.01
+    example: 200.01,
   })
   originalPrice: number;
 
   @ApiProperty({
     description: "积分",
-    example: 100
+    example: 100,
   })
   points: number;
 
   @ApiProperty({
     description: "赠送积分",
-    example: 100
+    example: 100,
   })
   givingPoints: number;
 
   @ApiProperty({
     description: "佣金比例",
-    example: 10.01
+    example: 10.01,
   })
   commissionRate: number;
 
   @ApiProperty({
     description: "库存",
-    example: 1000
+    example: 1000,
   })
   stock: number;
 
   @ApiProperty({
     description: "图片文件 ID 集合",
-    example: [1, 2, 3]
+    example: [1, 2, 3],
   })
   imageFileIds: number[];
 
   @ApiProperty({
     description: "描述",
-    example: "这是描述"
+    example: "这是描述",
   })
   description: string;
 
   @ApiProperty({
     description: "详情",
-    example: "这是详情"
+    example: "这是详情",
   })
   content: string;
 
   @ApiProperty({
     description: "是否新品",
-    example: Is.False
+    example: Is.False,
   })
   new: number;
 
   @ApiProperty({
     description: "是否推荐",
-    example: Is.False
+    example: Is.False,
   })
   recommended: number;
 
   @ApiProperty({
     description: "是否上架",
-    example: Is.True
+    example: Is.True,
   })
   status: number;
 
   @ApiProperty({
     description: "排序",
-    example: 1
+    example: 1,
   })
   order: number;
 
   @ApiProperty({
     description: "销量",
-    example: 1
+    example: 1,
   })
   sales: number;
+
+  @ApiPropertyOptional()
+  @ApiProperty({
+    description: "购买数量",
+    example: 1,
+  })
+  number?: number;
 }
